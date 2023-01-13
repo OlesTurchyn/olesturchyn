0c4886c9a16d81bfda54168d8bc8aa46a75f4a58
Oleksander Turchyn <oleksander.turchyn@gmail.com>
2023-01-13 15:16:19 -0500

    Modified Root Path and Built App

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/olesturchyn',
+      name: 'homepage',
+      component: HomeView
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
