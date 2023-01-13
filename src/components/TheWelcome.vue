468ff99449d96fcde638b9d43a97b82d97c80e06
Oleksander Turchyn <oleksander.turchyn@gmail.com>
2023-01-13 15:09:22 -0500

    Major Changes

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,68 +12,45 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    <template #heading>Geography Project</template>
+    <a href="https://olesturchyn.github.io/GeoTestVue/" target="_blank" rel="noopener">GeoTest.io</a>
+    is a small web project that was built with the intention of 
+    practicing some software engineering skills while studying geography.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>GitHub</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
+    This project is managed and hosted on my
+    <a href="https://github.com/OlesTurchyn" target="_blank" rel="noopener">GitHub</a> repo. Please check it out to 
+    see some of my other projects!
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <EcosystemIcon />
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading>Linkedin</template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    See my <a href="https://www.linkedin.com/in/oleksander-turchyn/" target="_blank" rel="noopener">Linkedin</a> 
+    for more info about my work experience and education. 
+    
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Community</template>
+    <template #heading>Contact</template>
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
+    If you'd like to get in touch, please reach out to me by email. 
+    <a href="mailto: oleksander.turchyn@gmail.com" target="_blank" rel="noopener">email</a>
   </WelcomeItem>
 
-  <WelcomeItem>
+  <!-- <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
@@ -82,5 +59,5 @@ import SupportIcon from './icons/IconSupport.vue'
     As an independent project, Vue relies on community backing for its sustainability. You can help
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  </WelcomeItem> -->
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>Hi, my name is Oles and I'm interested in Software Engineering, 
+      Cyber Security, and Business Intelligence. I aim to help
+      build solutions that can protect organizations and institutions from 
+      emerging threats.
+
+      <br>
+
+      <br>
+      I have internship experience in both the Public and Private sector where I've 
+      worked on threat intelligence research, full-stack web development, and IT Management.
+      
+      <br>
+      
+      <br> I'm based in Canada, but love exploring the world and trying new things. 
+      Some of my hobbies include photography, alpine skiing, volleyball, and dance. 
+
+      <br>
+
+      <br>
+
+      Thank you for checkign out my site!
+    </h3>
+
   </div>
 </template>
 
