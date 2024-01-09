12cc3888d7beda95e282d92ad45b0543d08c5f2f
Oles Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2024-01-09 13:42:29 -0500

    Added analytics

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,12 +5,9 @@ import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 
-
-
 </script>
 
 <template>
- 
 
   <WelcomeItem>
     <template #icon>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,30 @@ import Particles from "vue3-particles";
 
 import './assets/main.css'
 
+
+// FIREBASE SDK ANALTICS
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4Flu2aeo-UZI1yWCw6RKFtt3hXCCYwa4",
+  authDomain: "olesturchyn.firebaseapp.com",
+  projectId: "olesturchyn",
+  storageBucket: "olesturchyn.appspot.com",
+  messagingSenderId: "316261042539",
+  appId: "1:316261042539:web:3760b62f63e9f8622aa42e",
+  measurementId: "G-QJ64MMDB8L"
+};
+
+// Initialize Firebase
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 const app = createApp(App)
 
 app.use(router)
