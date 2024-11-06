946c4c7c46e253878963773aa8682a4bc6b3f1ac
Oles Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2024-11-06 10:50:37 -0500

    Updated research interests

--- src/views/ResearchView.vue
+++ src/views/ResearchView.vue
@@ -1,33 +1,37 @@
 <template>
-    <div class="research">
-      <br />
-      
-      <h2 style="color: white;">Research Interests</h2>
-      <ul>
-        <li>Graph Mining and Analytics</li>
-        <li>Machine learning for cyber security</li>
-      </ul>
+  <div class="research">
+    <br />
 
-      <br />
+    <h2 style="color: white">Research Interests</h2>
+    <ul>
+      <li>Geomatics and geospatial data mining</li>
+      <li>
+        Machine learning classification with convolutional neural networks
+      </li>
+    </ul>
 
-      <h2 style="color: white;">Skills</h2>
-      <ul>
-        <li>Full stack web development (Node.js, Express.js, MongoDB, Vue.js/React.js)</li>
-        <li>Python, Java, JavaScript, and Go</li>
-        <li>Threat intelligence research and development</li>
-        <li>Cloud development and reliability (AWS & GCP)</li>
-        <li>Customer-first mentality</li>
-      </ul>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
+    <br />
+
+    <h2 style="color: white">Skills</h2>
+    <ul>
+      <li>
+        Full stack web development (Node.js, Express.js, MongoDB,
+        Vue.js/React.js)
+      </li>
+      <li>Python, Java, JavaScript, and Go</li>
+      <li>Threat intelligence research and development</li>
+      <li>Cloud development and reliability (AWS & GCP)</li>
+      <li>Customer-first mentality</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
   }
-  </style>
-  
+}
+</style>
